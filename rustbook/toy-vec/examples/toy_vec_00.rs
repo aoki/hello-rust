33200ef9737823bc05630a17e2bc35ff16816574
aoki <ringohub@gmail.com>
2021-09-02 22:32:18 +0900

    Add len() and capacity()

--- rustbook/toy-vec/examples/toy_vec_00.rs
+++ rustbook/toy-vec/examples/toy_vec_00.rs
@@ -2,5 +2,7 @@ use toy_vec::ToyVec;
 
 fn main() {
     let v = ToyVec::<String>::new();
-    dbg!(v);
+    dbg!(&v);
+    println!("Length: {}", v.len());
+    println!("Capacity: {}", v.capacity());
 }
--- rustbook/toy-vec/src/lib.rs
+++ rustbook/toy-vec/src/lib.rs
@@ -9,7 +9,7 @@ impl<T: Default> ToyVec<T> {
     fn allocate_in_heap(size: usize) -> Box<[T]> {
         std::iter::repeat_with(Default::default)
             .take(size)
-            .collect()
+            .collect() // 本当は collect::<Vec<_>>() だけど実験
     }
 
     // 指定された capacity を持つ ToyVec を作る
@@ -23,5 +23,14 @@ impl<T: Default> ToyVec<T> {
     // キャパシティが0のToyVecを作成する
     pub fn new() -> Self {
         Self::with_capacity(0)
+    }
+
+    pub fn len(self: &Self) -> usize {
+        self.len
+    }
+
+    // `&self` は `self: &Self` の糖衣構文
+    pub fn capacity(&self) -> usize {
+        self.elements.len()
     }
 }
